0e291a4a4171fb400224406286644b6fe54d7bcc
nemorap <maxaceher@hotmail.com>
2016-06-10 17:55:04 -0500

    solucion_guitarra

---
+++ Estudiantes/guillermo/solucion_guitarra/software/nios_2/obj/default/karplus/karplus.d
@@ -0,0 +1,13 @@
+obj/default/karplus/karplus.o: karplus/karplus.c karplus/karplus.h \
+ ../nios_2_bsp//HAL/inc/io.h ../nios_2_bsp//HAL/inc/alt_types.h \
+ ../nios_2_bsp/system.h ../nios_2_bsp/linker.h
+
+karplus/karplus.h:
+
+../nios_2_bsp//HAL/inc/io.h:
+
+../nios_2_bsp//HAL/inc/alt_types.h:
+
+../nios_2_bsp/system.h:
+
+../nios_2_bsp/linker.h:
