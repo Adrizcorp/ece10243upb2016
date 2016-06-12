10299b43c609d77c2915cdb367dc58b9c2a7d4f3
nemorap <maxaceher@hotmail.com>
2016-06-12 12:32:37 -0500

    himno_de_la_alegria_guitarra

---
+++ Estudiantes/guillermo/solucion_himnodelaalegria_karplus_guitarra/software/Practica_nios_2/obj/default/hello_world.d
@@ -0,0 +1,12 @@
+obj/default/hello_world.o: hello_world.c \
+ ../Practica_nios_2_bsp//HAL/inc/io.h \
+ ../Practica_nios_2_bsp//HAL/inc/alt_types.h \
+ ../Practica_nios_2_bsp/system.h ../Practica_nios_2_bsp/linker.h
+
+../Practica_nios_2_bsp//HAL/inc/io.h:
+
+../Practica_nios_2_bsp//HAL/inc/alt_types.h:
+
+../Practica_nios_2_bsp/system.h:
+
+../Practica_nios_2_bsp/linker.h:
