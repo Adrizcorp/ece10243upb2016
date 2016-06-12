0568e4d210379d3eb505b109e2977b2506935f5b
chungalopez <chucholopez0423@gmail.com>
2016-06-12 13:59:11 -0500

    solucion_tabla_karpus

---
+++ Estudiantes/Jesus/Tabla_karplus_nios/karpus_sof/Tabla_karpus/obj/default/karpus/karplus.d
@@ -0,0 +1,14 @@
+obj/default/karpus/karplus.o: karpus/karplus.c karpus/karplus.h \
+ ../Tabla_karpus_bsp//HAL/inc/io.h \
+ ../Tabla_karpus_bsp//HAL/inc/alt_types.h ../Tabla_karpus_bsp/system.h \
+ ../Tabla_karpus_bsp/linker.h
+
+karpus/karplus.h:
+
+../Tabla_karpus_bsp//HAL/inc/io.h:
+
+../Tabla_karpus_bsp//HAL/inc/alt_types.h:
+
+../Tabla_karpus_bsp/system.h:
+
+../Tabla_karpus_bsp/linker.h:
