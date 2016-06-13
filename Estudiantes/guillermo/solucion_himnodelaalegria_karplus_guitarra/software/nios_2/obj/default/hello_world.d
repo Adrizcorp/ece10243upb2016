f64ee76dc46376e4952608bc06c9ffbce3688a37
nemorap <maxaceher@hotmail.com>
2016-06-13 01:14:19 -0500

    sotfware

---
+++ Estudiantes/guillermo/solucion_himnodelaalegria_karplus_guitarra/software/nios_2/obj/default/hello_world.d
@@ -0,0 +1,13 @@
+obj/default/hello_world.o: hello_world.c karplus/karplus.h \
+ ../nios_2_bsp//HAL/inc/io.h ../nios_2_bsp//HAL/inc/alt_types.h \
+ ../nios_2_bsp/system.h ../nios_2_bsp/linker.h
+
+karplus/karplus.h:
+
+../nios_2_bsp//HAL/inc/io.h:
+
+../nios_2_bsp//HAL/inc/alt_types.h:
+
+../nios_2_bsp/system.h:
+
+../nios_2_bsp/linker.h:
