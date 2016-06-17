c339a8cab5019cf64be80f28a6c0aa9651e02431
Adrizcorp <Adrizcorp@gmail.com>
2016-06-17 13:17:03 -0500

    FIR

--- Prof_Examples/4-5_FIR/FIR/FIR_filter.m
+++ Prof_Examples/4-5_FIR/FIR/FIR_filter.m
@@ -1,7 +1,7 @@
 % low_pass or Band_pass filters with windows Blackman Harris
 clear all
 clc
-Nf=70     % orden del filtro 
+Nf=24     % orden del filtro 
 N = Nf+1; % N?meros de Coeficientes 
 n = 0;
 fs = 44100;   % Frecuencia de muestreo 
@@ -13,9 +13,15 @@ M = floor(N/2)
   
  
 for i = 1:N  
- %hamming
- w(i)= 0.54-0.46*(cos((2*pi*n)/(N)));
- %w(i)= 0.42-0.5*(cos((2*pi*n)/(N-1)))+0.08*(cos((4*pi*n)/(N-1)));
+%  if(n<=N/2 && n>=0)
+%     w(i)= 2*n/N;
+%  else
+%      w(i)= 2-2*n/N;
+%  end
+%  
+  %w(i)= 0.5*(1-(cos((2*pi*n)/(N))));
+% w(i)= 0.54-0.46*(cos((2*pi*n)/(N)));
+ w(i)= 0.42-0.5*(cos((2*pi*n)/(N-1)))+0.08*(cos((4*pi*n)/(N-1)));
  % w(i) = (0.35875-0.48829*cos((2*pi*n)/(N-1))+0.14128*cos((4*pi*n)/(N-1))-0.01168*cos((6*pi*n)/(N-1)) );  
  if n ~= M
    hd(i) = ((sin(Wc1*((n)-M)))/(pi*((n)-M))) - ((sin(Wc2*((n)-M)))/(pi*((n)-M))) ;
